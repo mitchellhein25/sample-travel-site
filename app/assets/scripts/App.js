054886e4ff619782dadc92481e6934fbea524354
Mitch Hein <mitchelldalehein25@gmail.com>
2020-01-16 09:53:03 -0600

    Completed the header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,23 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
 }
+
+/*Lesson example code below this line */
+
+/*class Adult extends Person { inherits everything from Person class
+    payTaxes() {
+        console.log(this.name + " now owes zero taxes.");
+    }
+}
+
+let john = new Person("John Doe", "purple");
+john.greet();
+
+let jane = new Adult("Jane Smith", "green");
+jane.greet();
+jane.payTaxes();*/
