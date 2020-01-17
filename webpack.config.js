fb5e9d65b1a8e1f95922801183361e97e6a032da
Mitch Hein <mitchelldalehein25@gmail.com>
2020-01-17 10:24:25 -0600

    Completed the build process configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -9,7 +14,87 @@ const postCSSPlugins = [
     require('autoprefixer'),
 ]
 
-module.exports = {
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy Images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+    test: /\.css$/i, 
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+let config = {
+    entry: './app/assets/scripts/App.js',
+    plugins: pages,
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+}
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader')
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname,'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+    config.mode = 'development'
+}
+
+if (currentTask == 'build') {
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
+    cssConfig.use.unshift(MiniCSSExtractPlugin.loader)
+    postCSSPlugins.push(require('cssnano'))
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new MiniCSSExtractPlugin({filename: 'syles.[chunkhash].css'}),
+        new RunAfterCompile()
+    )
+}
+
+/* let deleteMeLater = {
     entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
@@ -17,8 +102,8 @@ module.exports = {
     },
     devServer: {
         before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
+            server._watch('./app/**//*.html')*/
+       /* },
         contentBase: path.join(__dirname,'app'),
         hot: true,
         port: 3000,
@@ -33,4 +118,6 @@ module.exports = {
             }
         ]
     }
-}
+}*/
+
+module.exports = config
